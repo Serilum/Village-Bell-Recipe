d9e230a40fe2ef307ebe2b936fc2dfe8c89fb832
Rick South <rick@natamus.com>
2024-07-28 07:10:53 +0200

    (v3.6) Added Bundle support. The mod can now be enabled/disabled via bundle configs. [+1]

--- Common/src/main/java/com/natamus/villagebellrecipe/util/Reference.java
+++ Common/src/main/java/com/natamus/villagebellrecipe/util/Reference.java
@@ -3,6 +3,6 @@ package com.natamus.villagebellrecipe.util;
 public class Reference {
 	public static final String MOD_ID = "villagebellrecipe";
 	public static final String NAME = "Village Bell Recipe";
-	public static final String VERSION = "3.5";
+	public static final String VERSION = "3.6";
 	public static final String ACCEPTED_VERSIONS = "[1.21.0]";
 }
